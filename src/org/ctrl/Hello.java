24426c9eb7fe444d52f35936fbdb21745a49f999
subashsubash2502 <subashsubash2502.subash3979@gmail.com>
2022-09-03 21:44:01 +0530

    task

---
+++ src/org/ctrl/Hello.java
@@ -0,0 +1,21 @@
+package org.ctrl;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+	// simple if 
+		
+		int age = 18;
+		
+	// 18 > 18(-+)
+		
+		if(age>=18) {
+			System.out.println("you are eligible to vote ");
+			
+		}
+		
+	}
+
+	
+}
